db6ffeb0a111045f7bd8664e8db73e44bba2356f
Mark Chavez <markchav3z@gmail.com>
2014-09-20 22:58:53 +0800

    singleton hooks

--- hooks.rb
+++ hooks.rb
@@ -6,8 +6,27 @@ class Person
   def self.method_added(method)
     puts "New method added - #{method}"
   end
+
+  def self.method_undefined(method)
+    puts "Method undefined - #{method}"
+  end
+
+  def self.method_removed(method)
+    puts "Method removed - #{method}"
+  end
+
+  def self.singleton_method_added(method)
+    puts "Singleton method added- #{method}"
+  end
 end
 
 class Employee < Person
   def name; end
+  def self.awesome; end
+  # undef_method :name
+  remove_method :name
+end
+
+def Employee.party!
+  "party!"
 end
