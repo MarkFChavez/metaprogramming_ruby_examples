d5153549fba4fa5f83693b2ef656354886970926
Mark Chavez <markchav3z@gmail.com>
2014-09-21 08:39:52 +0800

    hook with instance

--- hook_class_method.rb
+++ hook_class_method.rb
@@ -7,9 +7,13 @@ module Hook
       klass.extend ClassMethods
     end
 
+    def this_is_an_instance_method
+      "instance method"
+    end
+
     module ClassMethods
       def this_is_a_class_method
-        true
+        "class method"
       end
     end
   end
@@ -21,6 +25,11 @@ end
 
 class TestHookClassMethods < MiniTest::Test
   def test_that_it_has_new_class_methods
-    assert_equal true, Person.this_is_a_class_method
+    assert_equal "class method", Person.this_is_a_class_method
+  end
+
+  def test_that_it_has_instance_method
+    assert_equal "instance method", Person.new.this_is_an_instance_method
   end
 end
+
